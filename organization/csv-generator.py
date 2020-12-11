2fe275580e151a0ac9ede8d87a8dcdf8627a5307
Henrique Tavares Aguiar <ricktavares76@gmail.com>
2020-12-11 18:33:01 -0300

    organizer & classification

---
+++ organization/csv-generator.py
@@ -0,0 +1,14 @@
+from os import path, listdir
+
+fruit_type = "abius"
+
+filenames = listdir(path.join(path.curdir, "..", "databases", fruit_type))
+filenames = list(filter(lambda filename: filename.endswith(".png"), filenames))
+
+try:
+    with open(path.join(path.curdir, "classification", f"{fruit_type}.csv"), "x") as csv:
+        csv.write("image,label\n" + "\n".join((filename + "," for filename in sorted(filenames))))
+
+except FileExistsError:
+    with open(path.join(path.curdir, "classification", f"{fruit_type}.csv"), "a") as csv:
+        csv.write("\n" + "\n".join((filename + "," for filename in sorted(filenames))))
---
+++ organization/organizer.py
@@ -0,0 +1,33 @@
+from os import walk, mkdir, path, sep as os_sep
+from shutil import move
+import csv
+
+fruit_type = "siriguelas"
+
+with open(path.join(path.curdir, "classification", f"{fruit_type}.csv")) as csv_file:
+    csv_reader = csv.reader(csv_file, delimiter=",")
+    csv_rows = [row for row in csv_reader]
+
+label_transform = {"A": "good", "B": "medium", "C": "bad"}
+
+for quality_class in label_transform.values():
+    try:
+        mkdir(path.join(path.curdir, "..", "databases", fruit_type, quality_class))
+    except:
+        pass
+
+for filename, label in csv_rows:
+    label = label_transform.get(label)
+
+    for root, dirs, files in walk(path.join(path.curdir, "..", "databases", fruit_type)):
+        if root.split(os_sep)[-1] == label:
+            continue
+
+        elif filename in files:
+            move(path.join(root, filename), path.join(path.curdir, "..", "databases", fruit_type, label))
+            print(
+                "moving:",
+                path.join(root, filename),
+                "to:",
+                path.join(path.curdir, "..", "databases", fruit_type, label),
+            )
