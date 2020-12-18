180e8df6d75171917e0c9a034ccb7c17254f88a2
Henrique Tavares Aguiar <ricktavares76@gmail.com>
2020-12-17 23:21:33 -0300

    changes to make cajus-amarelos work

--- image-pre-processing.py
+++ image-pre-processing.py
@@ -8,10 +8,12 @@ from matplotlib import pyplot as plt
 from cv2 import erode
 from tqdm import tqdm
 
-src = path.join(path.curdir, "raw-images", "gabirobas")  # Image source folder
-dest = path.join(path.curdir, "databases", "gabirobas")  # Processed images output folder
+src = path.join(path.curdir, "raw-images", "cajus-amarelos-4")  # Image source folder
+dest = path.join(path.curdir, "databases", "cajus-amarelos")  # Processed images output folder
 
 debugging_mode = False  # Debbuging images
+
+contour_mode = "color"  # Use 'color' (slower but more accurate) or 'binary' (faster but less accurate)
 
 try:
     mkdir(path.join(path.curdir, dest))
@@ -47,20 +49,26 @@ for index, filename in tqdm(
     cropped_img = exposure.rescale_intensity(cropped_img)
     cropped_img = filters.median(cropped_img)
 
-    grayscale_img = color.rgb2gray(cropped_img)
+    if contour_mode == "binary":
+        grayscale_img = color.rgb2gray(cropped_img)
 
-    seed = np.copy(grayscale_img)
-    seed[1:-1, 1:-1] = grayscale_img.max()
-    mask = grayscale_img
+        seed = np.copy(grayscale_img)
+        seed[1:-1, 1:-1] = grayscale_img.max()
+        mask = grayscale_img
 
-    eroded = morphology.reconstruction(seed, mask, method="erosion")
+        eroded = morphology.reconstruction(seed, mask, method="erosion")
 
-    no_bg_img = grayscale_img - eroded
+        no_bg_img = grayscale_img - eroded
 
-    inv_no_bg_img = util.invert(no_bg_img)
-    inv_no_bg_img = exposure.rescale_intensity(inv_no_bg_img)
-    inv_no_bg_img = exposure.equalize_adapthist(inv_no_bg_img)
-    inv_no_bg_img = filters.median(inv_no_bg_img, morphology.disk(10))
+        inv_no_bg_img = util.invert(no_bg_img)
+        inv_no_bg_img = exposure.rescale_intensity(inv_no_bg_img)
+        inv_no_bg_img = exposure.equalize_adapthist(inv_no_bg_img)
+        inv_no_bg_img = filters.median(inv_no_bg_img, morphology.disk(10))
+
+        pre_preocessed_img = inv_no_bg_img
+
+    elif contour_mode == "color":
+        pre_preocessed_img = filters.median(cropped_img, morphology.ball(10))
 
     s = np.linspace(0, 2 * np.pi, 500)
     r = (IMG_SIZE / 2) + (IMG_SIZE / 2 - 5) * np.sin(s)
@@ -68,7 +76,7 @@ for index, filename in tqdm(
     init = np.array([r, c]).T
 
     contour = segmentation.active_contour(
-        inv_no_bg_img, init, alpha=0.1, beta=1, coordinates="rc", max_iterations=10000
+        pre_preocessed_img, init, alpha=0.1, beta=0.1, coordinates="rc", max_iterations=5000
     )
 
     mask = draw.polygon2mask((IMG_SIZE, IMG_SIZE), contour)
@@ -80,10 +88,10 @@ for index, filename in tqdm(
     iterations = ceil(perimeter * 0.007)
     reduced_mask = erode(util.img_as_ubyte(mask), kernel, iterations=iterations)
 
-    new_img_eroded = cropped_img * reduced_mask[:, :, np.newaxis]
-    new_img_eroded = exposure.adjust_gamma(new_img_eroded)
-    new_img_eroded = exposure.rescale_intensity(new_img_eroded)
-    new_img_eroded = exposure.equalize_adapthist(new_img_eroded)
+    processed_img = cropped_img * reduced_mask[:, :, np.newaxis]
+    processed_img = exposure.adjust_gamma(processed_img)
+    processed_img = exposure.rescale_intensity(processed_img)
+    processed_img = exposure.equalize_adapthist(processed_img)
 
     finish = time()
 
@@ -97,17 +105,17 @@ for index, filename in tqdm(
         ax[0].set_title(f"Perimeter: {perimeter:.2f}")
         ax[0].set_axis_off()
 
-        ax[1].imshow(inv_no_bg_img, cmap="gray")
+        ax[1].imshow(pre_preocessed_img, cmap="gray")
         ax[1].set_axis_off()
 
         ax[2].imshow(reduced_mask, cmap="gray")
         ax[2].set_title(f"num of iterations: {iterations}")
         ax[2].set_axis_off()
 
-        ax[3].imshow(new_img_eroded)
+        ax[3].imshow(processed_img)
         ax[3].set_axis_off()
 
         plt.show()
 
-    io.imsave(path.join(dest, f"img_{index:04}.png"), util.img_as_ubyte(new_img_eroded))
+    io.imsave(path.join(dest, f"img_{index:04}.png"), util.img_as_ubyte(processed_img))
     tqdm.write(f"{filename} processed into img_{index:04}.png successfully within {(finish - start):.1f}s")
